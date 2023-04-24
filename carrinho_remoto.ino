7ef9e948a1800baf4dd4b105ab0a02a4287e18c6
Vinícius Bocanegra <108433157+BOCADARK@users.noreply.github.com>
2023-04-24 15:57:15 -0300

    Create carrinho_remoto.ino

---
+++ carrinho_remoto.ino
@@ -0,0 +1,123 @@
+//83 para
+//para a direita o bluetooth lê 82;
+//para a esquerda 76
+//para frente 70
+//para trás 66
+//119 desliga LED dianteiro
+//87 liga o LED dianteiro
+//117 desliga o LED traseiro
+//85 liga o LED traseiro
+//118 desliga a buzina
+//86 liga a buzina
+//120 desliga o pisca alerta
+//88 liga o pisca alerta
+
+#include <SoftwareSerial.h>
+SoftwareSerial bluetooth(10, 11);
+#define m1 4  //tras
+#define m2 3  //frente
+#define m3 9  //tras
+#define m4 8  //frente
+
+#define LED 7
+#define buzzer 5
+
+void parar() {
+  digitalWrite(m1, LOW);
+  digitalWrite(m2, LOW);
+  digitalWrite(m3, LOW);
+  digitalWrite(m4, LOW);
+}
+
+void tras() {
+  digitalWrite(m1, HIGH);
+  digitalWrite(m2, LOW);
+  digitalWrite(m3, HIGH);
+  digitalWrite(m4, LOW);
+}
+
+void frente() {
+  digitalWrite(m1, LOW);
+  digitalWrite(m2, HIGH);
+  digitalWrite(m3, LOW);
+  digitalWrite(m4, HIGH);
+}
+void direita() {
+  digitalWrite(m1, LOW);
+  digitalWrite(m2, 50);
+  digitalWrite(m3, LOW);
+  digitalWrite(m4, LOW);
+}
+void esquerada() {
+  digitalWrite(m1, LOW);
+  digitalWrite(m2, LOW);
+  digitalWrite(m3, LOW);
+  digitalWrite(m4, HIGH);
+}
+
+void buzina() {
+  digitalWrite(buzzer, HIGH);
+  /*delay(300);
+  digitalWrite(buzzer, HIGH);
+  delay(300);
+  digitalWrite(buzzer, HIGH);
+  delay(300);
+  digitalWrite(buzzer, HIGH);
+  delay(300);
+  digitalWrite(buzzer, HIGH);
+  delay(300);
+  digitalWrite(buzzer, HIGH);
+  delay(300);*/
+}
+
+void farol() {
+   digitalWrite(LED, HIGH);
+  }
+
+
+void setup() {
+  bluetooth.begin(9600);
+  Serial.begin(9600);
+  pinMode(m1, OUTPUT);
+  pinMode(m2, OUTPUT);
+  pinMode(m3, OUTPUT);
+  pinMode(m4, OUTPUT);
+  pinMode(LED, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+}
+
+void loop() {
+  int C = bluetooth.read();
+  Serial.println(C);
+
+  if (C == 83) {
+    parar();
+  }
+
+  if (C == 82) {
+    direita();
+  }
+  if (C == 76) {
+    esquerada();
+  }
+
+  if (C == 70) {
+    frente();
+  }
+
+  if (C == 66) {
+    tras();
+  }
+
+  if (C == 86) {
+    buzina();
+  } else if (C == 118){
+    digitalWrite(buzzer, LOW);
+  }
+
+  if (C == 87) {
+    farol();
+  } else if (C == 119){
+    digitalWrite(LED, LOW);
+  }
+}
